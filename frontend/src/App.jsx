bb5e364a2dc6ce1e7101b76054ae7856b69a95f4
Teri-anric <2005ahi2005@gmail.com>
2025-02-04 22:32:04 +0200

    Remove unused Chat page and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import Error404 from './pages/error404/Error404.jsx';
-import Chat from './pages/chat/Chat.jsx';
 import Home from './pages/home/Home.jsx';
 
 function App() {
     return (
         <Routes>
             <Route index element={<Home />} />
-            <Route path="/chat/:chatId" element={<Chat />} />
             <Route path="*" element={<Error404 />} />
         </Routes>
     );
